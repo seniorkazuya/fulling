9004e699179f8855821056b1d9fafe1e51fd2e21
seniorkazuya <lovemyfamily7114@gmail.com>
2025-11-09 00:05:15 +0800

    Fix ttyd startup command error (#53)

--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Start ttyd with true color support
-# -t: Set terminal type to xterm-direct (supports 24-bit true color)
+# -T: Set terminal type to xterm-direct (supports 24-bit true color)
 # -W: Enable websocket compression
-ttyd -t 'xterm-direct' -W bash
+ttyd -T xterm-direct -W bash
